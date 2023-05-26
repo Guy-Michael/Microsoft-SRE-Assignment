c03b0ad3b7d707e668854034fb74eea0b475e00e
Guy Rahamim <guy.danino5@gmail.com>
2023-05-27 01:30:59 +0300

    temporary debugs

--- handle-blobs.sh
+++ handle-blobs.sh
@@ -3,9 +3,10 @@ connectionStringA=$CONNECTION_STRING_A
 connectionStringB=$CONNECTION_STRING_B
 groupName=$GROUP_NAME
 vmName=$VM_NAME
-
+{
 az vm run-command invoke \
     -g "$groupName" \
     -n "$vmName" \
     --command-id RunShellScript \
     --script "docker pull guymichael275/dotnet-script" "docker run --env CONNECTION_STRING_A=$connectionStringA --env CONNECTION_STRING_B=$connectionStringB guymichael275/dotnet-script"
+} >> ~/Desktop/files/log.txt
--- utility-scripts/install-utilities.sh
+++ utility-scripts/install-utilities.sh
@@ -5,12 +5,14 @@ machineName=$VM_NAME
 resourceGroupName=$GROUP_NAME
 
 # Check if both can work in one script
+{
 az vm run-command invoke \
     -n "$machineName" \
     -g "$resourceGroupName" \
+    --command-id RunShellScript \
     --scripts "curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash" \
                 "curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh ./get-docker.sh"
-
+} >> ~/Desktop/files/log.txt
 
 # curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
